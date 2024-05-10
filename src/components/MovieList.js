9b6c14ded40711659b4d43d94f955e520fe6e3fb
Bhanu <bhanuteja.sadu2003@gmail.com>
2024-05-10 16:43:01 +0530

    error fixed

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import movies from '../movies.json'; 
 
 
 const MovieList = ({ searchQuery, filters, movies }) => {
